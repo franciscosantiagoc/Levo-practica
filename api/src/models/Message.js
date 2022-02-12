502d0763086d205ae6e2a3c584aa82eb6e30081d
MasterFSC <44105867+MasterFSC@users.noreply.github.com>
2022-02-11 23:35:22 -0600

    definiendo modelo de tabla mensajes

---
+++ api/src/models/Message.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define("",
+  {
+    messageid: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    message:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    note:{
+      type: DataTypes.STRING,
+    }
+  },
+  { timestamps: false }
+)};
